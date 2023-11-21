5137e70730197dec13c88e55708990acfc0d1a42
Jeremy <126707817+prymytyve@users.noreply.github.com>
2023-11-21 16:45:12 -0500

    Adds form for location input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./style.css";
 import { getWeather } from "./mod_getWeatherAsync";
+const form = document.querySelector("#form");
+const locationInput = document.querySelector("#location");
 
-getWeather("");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log(locationInput.value);
+  await getWeather(locationInput.value);
+  form.reset();
+});
+
+// getWeather("");
